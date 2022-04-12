352c50c86744c53ee56c6898efb16186a07e8e6d
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-04-12 17:05:54 +0200

    Add milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,21 +49,20 @@ Non è necessario creare date casuali
 Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
 */
 
-let post = [];
+let posts = [];
 
 // creare una funzione che restituisce un numero casuale
 function genRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
+};
 
 // creare un ciclo for che crea un oggetto con i tuoi valori e lo pusha ogni volta nell array
 
-for (let i = 0; i < 2; i++) {
-  // numero id
-  let randomNumber = genRandomNumber(1, 100);
-  //console.log(randomNumber);
+for (let i = 0; i < 3; i++) {
 
+  // creo numero per id
+  let id_number = genRandomNumber(1, 100);
+ 
   //creare un array di nomi da associare a nome autore
   let nomi_autori = [
     "Marco",
@@ -75,8 +74,8 @@ for (let i = 0; i < 2; i++) {
     "Federica",
     "Gabriella",
   ];
-  let nome_autore_random = Math.floor(Math.random() * nomi_autori.length);
-  let nome_autore = nomi_autori[nome_autore_random]; // nome random che arriva dall'array di nomi
+  let posizione_nome_autore = Math.floor(Math.random() * nomi_autori.length);
+  let nome_autore = nomi_autori[posizione_nome_autore]; // nome random che arriva dall'array di nomi
   //console.log(nome_autore);
 
   //creare data casuale
@@ -86,27 +85,45 @@ for (let i = 0; i < 2; i++) {
   let data_post = `${giorno}/${mese}/${anno}`;
 
   //creare un numero per i like random
-  let like = genRandomNumber(1, 1000);
+  let like_number = genRandomNumber(1, 1000);
 
+  // creare un oggetto posts
+   let post = {
+     id: i + 1,
+     nomeAutore: nome_autore,
+     fotoAutore: "https://unsplash.it/300/300?image=",
+     data: data_post,
+     testo:
+       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nostrum dolorem. Qui amet doloremque ipsam provident veniam fugit corporis.",
+     immagine: "https://picsum.photos/300/300",
+     like: like_number,
+   };
+
+     posts.push(post);
+   /* console.log(posts); */
+};
+
+//creare un ciclo forEach per ottenere informazioni dell'oggetto
+let post = posts.forEach((post) => {
   // creare una card (post) dove appendi tutti gli elementi random
-  const container_card = document.querySelector('.container');
+  const container_card = document.querySelector(".container");
   let card_markup = `
   <div class="card my-2" style="width: 30rem;">
 
             <div class="card-body">
                 <div class="card_top d-flex">
-                    <img src="https://unsplash.it/300/300?image=" alt="" class="img-fluid rounded-circle" width="40">
+                    <img src="${post.immagine}" alt="" class="img-fluid rounded-circle" width="40">
                     <div class="card_top_text">
-                        <div class="nome fw-bold">${nome_autore}</div>
-                        <div class="data">${data_post}</div>
+                        <div class="nome fw-bold">${post.nomeAutore}</div>
+                        <div class="data">${post.data}</div>
                     </div>
                 </div>
 
                 <p class="card-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nostrum dolorem. Qui amet doloremque ipsam provident veniam fugit corporis.
+                    ${post.testo}
                 </p>
             </div>
-            <img src="https://unsplash.it/300/300?image=" class="card-img-top p-4" alt="...">
+            <img src="${post.fotoAutore}" class="card-img-top p-4" alt="...">
 
             <div class="button_card d-flex justify-content-around mb-3">
                 <a href="#" class="like">
@@ -116,13 +133,36 @@ for (let i = 0; i < 2; i++) {
                 <a href="#" class="like">
                     <span>
                         Piace a 
-                        <strong>${like} </strong>
+                        <strong>${post.like} </strong>
                         Persone
                     </span>
                 </a>
             </div>
         </div>
   `;
-  container_card.insertAdjacentHTML('beforeend', card_markup)
+  /* Milestone 2
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed. */
+  container_card.insertAdjacentHTML("beforeend", card_markup);
+});
 
-}
+
+
+
+
+
+
+/* 
+Milestone 3
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+*/
+
+// ogni volta che clicco il mio like deve colorarsi di blu e il numero dei like deve aumentare
+
+//creo un eventListener che ascolta il click
+
+/* let like = document.querySelector('.like');
+like.addEventListener('click', function() {
+    
+}) */
+
+console.log(posts);
